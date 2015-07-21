887b45a1667ac3bb8143cf9c8d843b2dc98f9748
Nicole Carlson <nicole@parsingscience.com>
2015-07-21 17:50:24 -0500

    Better comments for band_graph.js

--- pages/projects/enter_music/js/band_graph.js
+++ pages/projects/enter_music/js/band_graph.js
@@ -1,17 +1,23 @@
 // band_graph.js: Sets up graphs for bands and venues
 // Nicole Carlson
 
-// Create Band Graph
+
 $(function() {
 
+  // Add a circle to the Band Graph
 	Concerts.AddCircle=function(id, radius, x, y){
+    /* Parameters:
+    id: id for the circle, usually the band name
+    radius: radius of the circle
+    x,y: initial coordinates of the circle
+    */
 
-		// Overall g element for segments
+		// Overall g element for circle
     var bandCircle = d3.select("#band-graph")
       .append("svg:g")
       .data([{"x":x, "y": y}])
       .attr("transform", "translate(" + x + "," + y + ")")
-      .attr("id", "segmentG"+name)
+      .attr("id", id)
       .call(onDragDrop(dragmove));
 
 		var circle=bandCircle.append("svg:circle")
@@ -45,8 +51,10 @@ $(function() {
       // console.log("d3.event.dx: "+d3.event.dx)
       // console.log("d3.event.dy: "+d3.event.dy)
 
+      //Prevent object from being dragged offscreen.
       var tempX=Math.max(0, Math.min(300, d.x))
       var tempY=Math.max(0, Math.min(300, d.y))
+
       // console.log("d.x: "+d.x)
       // console.log("d.y: "+d.y)
       // console.log("tempX: "+tempX)
@@ -55,9 +63,15 @@ $(function() {
     }
 	}
 
-	Concerts.AddRectangle=function(id, radius, x, y){
+  //Add a rectangle to the venue graph (this will be a square most of the time)
+	Concerts.AddRectangle=function(id, width, x, y){
+    /* Parameters:
+    id: id for the rectangle, usually the venue name
+    width: width of the rectangle
+    x,y: initial coordinates of the rectangle
+    */
 
-		// Overall g element for segments
+		// Overall g element for rectangle
     var venueSquare = d3.select("#venue-graph")
       .append("svg:g")
       .data([{"x":x, "y": y}])
@@ -66,15 +80,15 @@ $(function() {
       .call(onDragDrop(dragmove));
 
 		var square=venueSquare.append("svg:rect")
-			.attr("height", 10*radius)
-			.attr("width", 10*radius)
+			.attr("height", 10*width)
+			.attr("width", 10*width)
 			.attr("stroke", "gray")
 			.attr("fill", "#F9C22E")
 
 		var text = venueSquare.append("svg:text")
       .text(id)
-      .attr("x", 5*radius)
-      .attr("y", 5*radius)
+      .attr("x", 5*width)
+      .attr("y", 5*width)
       .attr("text-anchor", "middle")
       .attr("font-weight", 700)
       .attr("font-size", 5)
@@ -95,8 +109,10 @@ $(function() {
 
       //console.log(d3.select(this).data())
 
+      //Prevent object from being dragged offscreen.
       var tempX=Math.max(0, Math.min(295, d.x))
       var tempY=Math.max(0, Math.min(295, d.y))
+      
       // console.log("d.x: "+d.x)
       // console.log("d.y: "+d.y)
       // console.log("tempX: "+tempX)
